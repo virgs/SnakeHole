df6e1a7d4632af3f9cbb79a45059c9747b89a8f7
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-08-04 12:57:14 -0300

    Adjust border thickness

--- src/scale.ts
+++ src/scale.ts
@@ -4,7 +4,7 @@ const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 const [dimensionWidth, dimensionHeight] = new UrlQueryHandler().getParameterByName('levelDimension', '25,25').split(',');
 
-const borderThickness = 2;
+const borderThickness = 0.5;
 const ratio = Math.min(windowHeight / dimensionHeight, windowWidth / dimensionWidth) - borderThickness;
 export const scale: number = Math.min(ratio, 30);
 console.log(scale);
